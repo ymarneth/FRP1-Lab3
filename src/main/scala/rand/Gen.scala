93ad24de55265be3232eabe37b79dee9219786a5
Yvonne Marneth <marneth.yvonne@gmail.com>
2024-11-08 11:36:36 +0100

    Implement Generator

--- src/main/scala/rand/Gen.scala
+++ src/main/scala/rand/Gen.scala
@@ -1,33 +1,41 @@
 package rand
 
-import stream._
+import rand.Gen.intsFromTo
+import stream.*
 
 trait Gen[A] extends (Long => (A, Long)) {
 
-  def flatMap[B](f: A => Gen[B]) : Gen[B] =
+  def flatMap[B](f: A => Gen[B]): Gen[B] =
     seed => {
-      val (a, nextSeed) = this(seed)
+      val (a, nextSeed) = this (seed)
       f(a)(nextSeed)
     }
 
-  def map[B](f: A => B) : Gen[B] =
+  def map[B](f: A => B): Gen[B] =
     this.flatMap(a => Gen.unit(f.apply(a)))
 
   // TODO: Task 8.3.a)
-  def lists(len: Int): Gen[List[A]] = ???
+  def lists(len: Int): Gen[List[A]] =
+    if len == 0 then Gen.unit(List.empty)
+    else
+      for {
+        ra <- this
+        rl <- lists(len - 1)
+      } yield ra :: rl
 
   // TODO: Task 8.3.b)
-  def listsOfLengths(minLen: Int, maxLen: Int): Gen[List[A]] = ???
+  def listsOfLengths(minLen: Int, maxLen: Int): Gen[List[A]] =
+    intsFromTo(minLen, maxLen).flatMap(len => lists(len))
 
-  // TODO: Tast 9.5
-  def stream(seed: Long) : Stream[A] = ???
+  // TODO: Task 9.5
+  def stream(seed: Long): Stream[A] = ???
 
-  def stream : Stream[A] = stream(System.currentTimeMillis())
+  def stream: Stream[A] = stream(System.currentTimeMillis())
 }
 
 object Gen {
 
-  def unit[A](a: A) : Gen[A] = (seed => (a, seed))
+  private def unit[A](a: A): Gen[A] = seed => (a, seed)
 
   val ints: Gen[Int] = seed => {
     val randInt = (seed >>> 16).toInt
@@ -36,24 +44,33 @@ object Gen {
   }
 
   // TODO: Task 8.1: Random number generators
-  val posInts: Gen[Int] = ???
-  def intsFromTo(from: Int, to: Int) : Gen[Int] = ???
-  def intsTo(to: Int) : Gen[Int] = ???
+  val posInts: Gen[Int] = ints.map(i => i.abs)
 
-  val doubles: Gen[Double] = ???
-  def doublesFromTo(from: Double, to: Double) : Gen[Double] = ???
-  def doublesTo(to: Double) : Gen[Double] = ???
+  def intsFromTo(from: Int, to: Int): Gen[Int] =
+    doubles.map(d => from + (d * (to - from)).toInt)
+
+  def intsTo(to: Int): Gen[Int] = intsFromTo(0, to)
+
+  val doubles: Gen[Double] = posInts.map(i => i.toDouble / Int.MaxValue.toDouble)
+
+  def doublesFromTo(from: Double, to: Double): Gen[Double] = ???
+
+  def doublesTo(to: Double): Gen[Double] = ???
 
   // TODO: Task 8.2: Discrete random values
 
-  def booleans(prob: Double) = ???
+  def booleans(prob: Double) = doubles.map(d => d < prob)
 
-  def valuesOf[A](values: A*) : Gen[A] = ???
+  def valuesOf[A](values: A*): Gen[A] = intsTo(values.length).map(i => values(i))
 
-  val letters : Gen[Char] = ???
-  //'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+  val letters: Gen[Char] = valuesOf(
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+    'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+    'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+  )
 
   // TODO: Task 8.3c: words
-  def words(len: Int) : Gen[String] = ???
-
+  def words(maxLength: Int): Gen[String] = letters
+    .listsOfLengths(2, maxLength)
+    .map(_.mkString)
 }
--- src/main/scala/rand/GenTests.scala
+++ src/main/scala/rand/GenTests.scala
@@ -5,9 +5,18 @@ import Gen.*
 object GenTests {
 
   def main(args: Array[String]): Unit = {
+    // ints.flatMap(i1 => ints.map(i2 => (i1, i2)))
+    val pairGen =
+      for {
+        i1 <- ints
+        i2 <- ints
+      } yield (i1, i2)
+
+    val rp = pairGen(574934312343L)
+    println(rp)
 
     // TODO: Task 8.4.a) Generator of list of 10 Int values from 0 to 100
-    val b10: Gen[List[Boolean]] = ???
+    val b10: Gen[List[Boolean]] = booleans(0.5).lists(10)
     val (bs, _) = b10(3293)
     for (b <- bs) println(b)
     println
@@ -19,7 +28,7 @@ object GenTests {
     println
 
     // TODO: Task 8.4.c) Generator for list with 10 lists with lenght between 2 and 10 of integers from 0 to 100
-    val nIntListLists:  Gen[List[List[Int]]] = ???
+    val nIntListLists: Gen[List[List[Int]]] = ???
     val (r1, _) = nIntListLists(34243)
     for (l <- r1)
       println(l)
