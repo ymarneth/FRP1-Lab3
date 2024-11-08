2f5709e6a7f18bdfbf1c597ab4f4b8b172cfa3ef
Yvonne Marneth <marneth.yvonne@gmail.com>
2024-11-08 09:44:43 +0100

    Implement Reducible

--- src/main/scala/reduce/Reducible.scala
+++ src/main/scala/reduce/Reducible.scala
@@ -15,7 +15,12 @@ trait Reducible[A] {
 
 object Reducible {
 
-  def apply[A](as: Iterable[A]): Reducible[A] = ???
+  def apply[A](as: Iterable[A]): Reducible[A] = new Reducible[A]:
+    override def reduceMap[B](mapper: A => B)(using monoid: Monoid[B]): B = {
+      var result = monoid.zero
+      for a <- as do result = monoid.op(result, mapper(a))
+      result
+    }
 
   def apply[A](tree: BinTree[A]): Reducible[A] = ???
 
--- src/main/scala/reduce/Task6_2_ListReducible.scala
+++ src/main/scala/reduce/Task6_2_ListReducible.scala
@@ -12,20 +12,21 @@ object Task6_2_ListReducible {
     // === Task 6.2 ====================
 
     //a) count the elements
-    val n = ???
+    val n = namesReducible.reduceMap(_ => 1) //intPlusMonoid
     println(s"Number elements = $n")
 
     //b) concatenate the elements to a single string
-    val one = ???
+    val one = namesReducible.reduce
     println(s"Concatenated = $one")
 
     //c) compute length of all strings
-    val length = ???
+    val length = namesReducible.reduceMap(_.length) //intPlusMonoid
     println(s"Length of elements = $length")
 
     //d) create a set of the elements
-    val setOfNames = ???
+    val setOfNames: Set[String] = namesReducible.reduceMap(name => Set(name))(using setMonoid[String]()) //setMonoid
     println(s"Set of elements = $setOfNames")
-  }
 
+    val maxLength = namesReducible.reduceMap(name => name.length)(using Monoid(0, (l1, l2) => if l1 > l2 then l1 else l2))
+  }
 }
