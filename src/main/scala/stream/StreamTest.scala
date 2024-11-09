0d4949ee1283ba54e3f7c75cde3cb2e1be93a32b
Yvonne Marneth <marneth.yvonne@gmail.com>
2024-11-09 23:04:46 +0100

    Implement exercise 9

--- src/main/scala/stream/StreamTest.scala
+++ src/main/scala/stream/StreamTest.scala
@@ -4,34 +4,38 @@ object StreamTest extends App {
 
   import Stream._
 
-  // TODO:  Task 9.4.a) Use iterate for defining an infinite stream of positive integers,
+  // Task 9.4.a Use iterate for defining an infinite stream of positive integers,
   //    filter out odd values, map them to their square and print out the first 10.
-  val intsFrom1: Stream[Int] = iterate(1, i => i + 1)
-  println(intsFrom1.headOption)
-  println(intsFrom1.tailOption.get.headOption)
+  private val intsFrom1: Stream[Int] =
+    iterate(1, i => i + 1).filter(_ % 2 == 0).map(i => i * i).take(10)
+  for (i <- intsFrom1.toList) {
+    println(i)
+  }
   println
 
-  // TODO:  Task 9.4.b)	Use iterate for defining an infinite stream of powers of 2 starting by 2.
+  // Task 9.4.b	Use iterate for defining an infinite stream of powers of 2 starting by 2.
   //    Take the first 10 and create a list.
-  val powerOf2: List[Int] = ???
-  println(powerOf2)
+  private val powerOf2: List[Int] = iterate(2, i => i * 2).take(10).toList
+  powerOf2.foreach(println)
   println
 
-  val words = List("Hannah", "Ann", "Sepp", "Peter", "", "Paul", "Flora")
-  // TODO:  Task 9.4.c)	Use from for creating a Stream from a list of words, filter out those of length 0,
+  // Task 9.4.c)	Use from for creating a Stream from a list of words, filter out those of length 0,
   //    map them to their length and print out the result
+  val words = List("Hannah", "Ann", "Sepp", "Peter", "", "Paul", "Flora", "Nick")
+  private val wordLengths: Stream[Int] = from(words).filter(_.nonEmpty).map(_.length)
+  wordLengths.forEach(println)
   println
 
-  // TODO:  Task 9.4.d)	Use from for creating a Stream from a list of words and
+  // Task 9.4.d)	Use from for creating a Stream from a list of words and
   //    find a word which contain a "A" or "a"". Create a list.
-  val wordsA: List[String] = ???
-  println(wordsA)
+  private val wordsA: List[String] = from(words).filter(word => word.contains("A") || word.contains("a")).toList
+  wordsA.foreach(println)
+  println
 
-  // TODO:  Task 9.4.e)	Use iterate for creating a Stream of all integers
+  // Task 9.4.e	Use iterate for creating a Stream of all integers
   //  starting with 1000 and find the first prime number (Use filter and headOption)
-  val firstPrime: Option[Int] = ??? 
-  println(firstPrime)
-
+  private val firstPrime: Option[Int] = iterate(1000, i => i + 1).filter(isPrime).headOption
+  println(s"The first prime number: ${firstPrime.get}")
 }
 
 def isPrime(x: Int) : Boolean = {
